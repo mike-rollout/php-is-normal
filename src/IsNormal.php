c27845d05eaf7eeb4404001152c928d5279189ea
mike-rollout <michael@rollout.com>
2024-07-05 16:45:57 +0300

    first commit

---
+++ src/IsNormal.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YourNamespace;
+
+class IsNormal {
+    public static function getMotivationalQuote() {
+        $url = 'https://api.quotable.io/random?tags=humor';
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+        return $data['content'] ?? 'Stay positive and keep going!';
+    }
+
+    public static function isNormal() {
+        $randomAnswer = rand(0, 1) == 0 ? 'yes' : 'no';
+        $motivationalQuote = self::getMotivationalQuote();
+        return "$randomAnswer. $motivationalQuote";
+    }
+}
